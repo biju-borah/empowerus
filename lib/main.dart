4c2bbadc73efe2e0ebe7b4d19fce51988df16fc6
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-11-21 01:15:30 +0530

    Added stuffs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,41 +12,145 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'EmpowerUS',
-        home: Scaffold(
-            body: Container(
+      title: 'EmpowerUS',
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('hello'),
+            backgroundColor: Colors.blue,
+            elevation: 0,
+            leading: const Icon(Icons.arrow_back)),
+        body: Container(
           margin: const EdgeInsets.all(10),
           padding: const EdgeInsets.all(10),
           child: Form(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Hello'),
+                TextFormField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
                         borderSide: const BorderSide(
-                            color: Colors.greenAccent, width: 2.0),
-                        borderRadius: BorderRadius.circular(50.0)),
-                    enabledBorder: OutlineInputBorder(
+                          color: Colors.greenAccent,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50.0),
                         borderSide: const BorderSide(
-                            color: Colors.greenAccent, width: 2.0)),
-                    hintText: 'TheF'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
+                          color: Colors.greenAccent,
+                          width: 2.0,
+                        ),
+                      ),
+                      hintText: 'TheF'),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(
                     focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Colors.greenAccent, width: 2.0),
-                        borderRadius: BorderRadius.circular(50.0)),
+                      borderRadius: BorderRadius.circular(50.0),
+                      borderSide: const BorderSide(
+                        color: Colors.greenAccent,
+                        width: 2.0,
+                      ),
+                    ),
                     enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0),
-                        borderSide: const BorderSide(
-                            color: Colors.greenAccent, width: 2.0)),
-                    hintText: 'TheF2'),
-              )
-            ],
-          )),
-        )));
+                      borderRadius: BorderRadius.circular(50.0),
+                      borderSide: const BorderSide(
+                        color: Colors.greenAccent,
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'TheF2',
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: const BorderSide(
+                              color: Colors.greenAccent,
+                              width: 2.0,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: const BorderSide(
+                              color: Colors.greenAccent,
+                              width: 2.0,
+                            ),
+                          ),
+                          hintText: 'TheF2',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 50),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: const BorderSide(
+                              color: Colors.greenAccent,
+                              width: 2.0,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            borderSide: const BorderSide(
+                              color: Colors.greenAccent,
+                              width: 2.0,
+                            ),
+                          ),
+                          hintText: 'TheF2',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                      borderSide: const BorderSide(
+                        color: Colors.greenAccent,
+                        width: 2.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                      borderSide: const BorderSide(
+                        color: Colors.greenAccent,
+                        width: 2.0,
+                      ),
+                    ),
+                    hintText: 'TheF2',
+                  ),
+                ),
+                const Spacer(),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {},
+                  child: Text('TextButton'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
